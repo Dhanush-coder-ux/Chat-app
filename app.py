95571d7c97685bc3f3e92b287f3810fb4615622a
Dhanush Kumar <dhanushsubbia703@gmail.com>
2024-10-14 18:50:42 +0530

    app.py

--- app.py
+++ app.py
@@ -1,13 +1,12 @@
 from flask import Flask, render_template,request
 from flask_socketio import SocketIO, emit
 from flask_mysqldb import MySQL
-import uuid  # To generate unique IDs
+import uuid  
 import time
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'wertiuyhyuhuybygy7gggyg8yh8y'
 
-# MySQL configuration
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'Dhanush@2005#'
@@ -17,12 +16,12 @@ app.config['MYSQL_PORT'] = 3306
 mysql = MySQL(app)
 socketio = SocketIO(app)
 
-# Store user sessions and their unique IDs
+
 users = {}
 
 @app.route('/')
 def home():
-    # Retrieve all messages from the database
+   
     cur = mysql.connection.cursor()
     cur.execute("SELECT * FROM messages")
     messages = cur.fetchall()
@@ -31,9 +30,9 @@ def home():
 
 @socketio.on('connect')
 def handle_connect():
-    user_id = str(uuid.uuid4())  # Generate a unique ID for the user
-    users[user_id] = request.sid  # Store the unique ID with the session ID (Socket.IO connection ID)
-    emit('assign_user_id', {'user_id': user_id})  # Send the unique ID to the client
+    user_id = str(uuid.uuid4()) 
+    users[user_id] = request.sid 
+    emit('assign_user_id', {'user_id': user_id})  
     print(f"Client connected with user ID: {user_id}")
 
 @socketio.on('disconnect')
@@ -45,18 +44,18 @@ def handle_disconnect():
 
 @socketio.on('message')
 def handle_message(data):
-    user_id = data['user_id']  # Get the user's unique ID
+    user_id = data['user_id']  
     message_content = data['content']
     
     print(f"Received from user {user_id}: {message_content}")
     
-    # Save the message content to the database
+   
     cur = mysql.connection.cursor()
     cur.execute("INSERT INTO messages (content, user_id) VALUES (%s, %s)", (message_content, user_id))
     mysql.connection.commit()
     cur.close()
 
-    # Emit the response back to all clients
+   
     response_data = {'content': message_content, 'sender': user_id}
     emit('response', response_data, broadcast=True)
     
